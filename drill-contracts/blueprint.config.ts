86c09f4163daa33dc296ba2143945c9cf66fc0a2
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2026-08-28 17:44:17 +0700

    Use public Tonhub testnet RPC

--- drill-contracts/blueprint.config.ts
+++ drill-contracts/blueprint.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@ton/blueprint';
 
 export const config: Config = {
   network: {
-    endpoint: 'https://ton-testnet.core.chainstack.com/8e3c4c3c0b0a4f3e8f0c0b0a4f3e8f0c/jsonRPC',
+    endpoint: 'https://testnet.tonhubapi.com/jsonRPC',
     type: 'testnet',
     version: 'v2',
   },
